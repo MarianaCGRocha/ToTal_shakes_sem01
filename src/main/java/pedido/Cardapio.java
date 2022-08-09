34c49fb2a878814f1440c03a88ac68f7121d1136
Mariana Rocha <marianagr@ciandt.com>
2022-08-09 19:05:33 -0300

    test: final validation of cardapio test

--- src/main/java/pedido/Cardapio.java
+++ src/main/java/pedido/Cardapio.java
@@ -5,13 +5,14 @@ import ingredientes.TipoTopping;
 import ingredientes.Topping;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.TreeMap;
 
 public class Cardapio {
     private TreeMap<Ingrediente,Double> precos;
 
     public Cardapio(){
-        this.precos= new TreeMap<>();
+        this.precos= new TreeMap<>(Collections.reverseOrder());
     }
 
     public TreeMap<Ingrediente, Double> getPrecos(){
@@ -29,7 +30,6 @@ public class Cardapio {
 
     public boolean atualizarIngrediente(Ingrediente ingrediente,Double preco){
        //TODO
-
         if (preco > 0.0){
             if (this.precos.containsKey(ingrediente)) {
                 this.precos.replace(ingrediente, preco);
